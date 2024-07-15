91c2ad29dc4653917ccffd5ab8a35f791cd6e68f
BinomialSheep <jose_jinrou@yahoo.co.jp>
2024-07-15 14:52:15 +0900

    入出力確認

--- 01_naive.cpp
+++ 01_naive.cpp
@@ -1,8 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
-  cout << "Hello" << endl;
+/*
+入出力確認
+*/
 
-  return 0;
+int main() {
+  int id;  // id of your player.
+  cin >> id;
+  cin.ignore();
+  int board_size;
+  cin >> board_size;
+  cin.ignore();
+
+  // game loop
+  while (1) {
+    for (int i = 0; i < board_size; i++) {
+      string line;  // rows from top to bottom (viewer perspective).
+      cin >> line;
+      cin.ignore();
+    }
+    int action_count;  // number of legal actions for this turn.
+    cin >> action_count;
+    cin.ignore();
+    vector<string> actions;
+    for (int i = 0; i < action_count; i++) {
+      string action;
+      cin >> action;
+      cin.ignore();
+      actions.push_back(action);
+      cerr << actions.back() << endl;
+    }
+    if (actions.size()) cout << actions[0] << endl;  // a-h1-8
+  }
 }
